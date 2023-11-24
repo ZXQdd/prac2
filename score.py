6f91fa24e8c077797768f43485b102037cb75062
zhao0515 <110957684+zhao0515@users.noreply.github.com>
2023-11-24 13:55:54 +0800

    commit 1

---
+++ score.py
@@ -0,0 +1,24 @@
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(f'The score {score} is {determine_score_result(score)}')
+    generate_score = random.randint(1, 100)
+    print(f'The random score {generate_score} is {determine_score_result(generate_score)}')
+
+
+def determine_score_result(score):
+    """ Will determine the result """
+    if score > 100 or score < 0:
+        return 'Invalid score'
+    else:
+        if score >= 90:
+            return 'Excellent'
+        elif score >= 50:
+            return 'Pass'
+        else:
+            return 'Bad'
+
+
+main()
