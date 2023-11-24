40275b4a9ae74f361ea2c3f6d3428d690baa76a5
zhao0515 <110957684+zhao0515@users.noreply.github.com>
2023-11-24 13:48:34 +0800

    commit 1

---
+++ password_stars.py
@@ -0,0 +1,22 @@
+PASSWORD_LENGTH = 10
+
+
+def main():
+    get_password()
+    print_password()
+
+
+def get_password():
+    """ Will check for password length"""
+    user_password = input('Password: ')
+    while len(user_password) != PASSWORD_LENGTH:
+        print('Does not meet the requirement')
+        user_password = input('Password: ')
+
+
+def print_password():
+    """ Will print stars """
+    print('*' * 10)
+
+
+main()
